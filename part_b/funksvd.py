67cbf1d5d3281327e87a9706171784aeab04f533
Alex Shih <alex.alex.shih@gmail.com>
2020-12-15 01:25:08 -0500

    updated plots

--- part_b/funksvd.py
+++ part_b/funksvd.py
@@ -1,8 +1,37 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 from funk_svd import SVD
 from utils import *
+
+
+def svd(data, svd_data, lr=0.01, reg=0.1, k=10, iters=1000):
+    train_data, val_data = data['train_data'], data['val_data']
+    train_svd, val_svd = svd_data['train_svd'], svd_data['val_svd']
+
+    svd = SVD(learning_rate=lr, regularization=reg, n_epochs=iters, n_factors=k, min_rating=0, max_rating=1)
+    svd.fit(X=pd.DataFrame(train_svd), X_val=pd.DataFrame(val_svd), early_stopping=False, shuffle=False)
+
+    # Train Accuracy
+    pred = svd.predict(train_svd)
+    train_acc = evaluate(train_data, pred)
+
+    # Validate Accuracy
+    pred = svd.predict(val_svd)
+    val_acc = evaluate(val_data, pred)
+
+    return train_acc, val_acc
+
+
+def plot(x_vals, train_y, valid_y, label):
+    plt.plot(x_vals, train_y, '--ok', color='red', label="training")
+    plt.plot(x_vals, valid_y, '--ok', color='blue', label="validation")
+    plt.xlabel(label)
+    plt.ylabel("accuracy")
+    plt.title("Accuracy vs. " + label)
+    plt.legend()
+    plt.show()
 
 
 def main():
@@ -15,23 +44,76 @@ def main():
     val_svd = {'u_id': val_data['user_id'], 'i_id': val_data['question_id'], 'rating': val_data['is_correct']}
     test_svd = {'u_id': test_data['user_id'], 'i_id': test_data['question_id'], 'rating': test_data['is_correct']}
 
-    svd = SVD(learning_rate=0.01, regularization=0.01, n_epochs=1000, n_factors=30, min_rating=0, max_rating=1)
-    svd.fit(X=pd.DataFrame(train_svd), X_val=pd.DataFrame(val_svd), early_stopping=True, shuffle=False)
+    data = {"train_data": train_data, "val_data": val_data}
+    svd_data = {"train_svd": train_svd, "val_svd": val_svd}
+
+    lrs = [0.001, 0.005, 0.01, 0.05, 0.1, 0.2]
+    regs = [0.001, 0.01, 0.05, 0.08, 0.1, 0.15, 0.5, 0.75, 1]
+    ks = [1, 5, 10, 15, 20, 30, 50, 100]
+    iters = [1, 5, 10, 20, 50, 100, 300, 500, 700, 900, 1000, 2000]
+
+    lr_train_results = []
+    lr_val_results = []
+    reg_train_results = []
+    reg_val_results = []
+    ks_train_results = []
+    ks_val_results = []
+    iters_train_results = []
+    iters_val_results = []
+
+    for lr in lrs:
+        train_result, val_result = svd(data, svd_data, lr=lr)
+        lr_train_results.append(train_result)
+        lr_val_results.append(val_result)
+
+    for reg in regs:
+        train_result, val_result = svd(data, svd_data, reg=reg)
+        reg_train_results.append(train_result)
+        reg_val_results.append(val_result)
+
+    for k in ks:
+        train_result, val_result = svd(data, svd_data, k=k)
+        ks_train_results.append(train_result)
+        ks_val_results.append(val_result)
+
+    for iter in iters:
+        train_result, val_result = svd(data, svd_data, iters=iter)
+        iters_train_results.append(train_result)
+        iters_val_results.append(val_result)
+
+    print("LR TRAIN ACCURACIES: ", lr_train_results)
+    print("LR VAL ACCURACIES: ", lr_val_results)
+    plot(lrs, lr_train_results, lr_val_results, "Learning Rates")
+
+    print("REGULARIZER TRAIN ACCURACIES: ", reg_train_results)
+    print("REGULARIZER VAL ACCURACIES: ", reg_val_results)
+    plot(regs, reg_train_results, reg_val_results, "Regularized Rates")
+
+    print("K TRAIN ACCURACIES: ", ks_train_results)
+    print("K VAL ACCURACIES: ", ks_val_results)
+    plot(ks, ks_train_results, ks_val_results, "K-Values")
+
+    print("ITERS TRAIN ACCURACIES: ", iters_train_results)
+    print("ITERS VAL ACCURACIES: ", iters_val_results)
+    plot(iters, iters_train_results, iters_val_results, "Iterations")
+
+    final_svd = SVD(learning_rate=0.01, regularization=0.1, n_epochs=1000, n_factors=10, min_rating=0, max_rating=1)
+    final_svd.fit(X=pd.DataFrame(train_svd), X_val=pd.DataFrame(val_svd), early_stopping=False, shuffle=False)
 
     # Train Accuracy
-    pred = svd.predict(train_svd)
-    acc = evaluate(train_data, pred)
-    print("Train Accuracy: ", acc)
+    pred = final_svd.predict(train_svd)
+    train_acc = evaluate(train_data, pred)
+    print("Final Train Accuracy: ", train_acc)
 
     # Validate Accuracy
-    pred = svd.predict(val_svd)
-    acc = evaluate(val_data, pred)
-    print("Validation Accuracy: ", acc)
+    pred = final_svd.predict(val_svd)
+    val_acc = evaluate(val_data, pred)
+    print("Final Validation Accuracy: ", val_acc)
 
-    # Test accuracy
-    pred = svd.predict(test_svd)
-    acc = evaluate(test_data, pred)
-    print("Test Accuracy: ", acc)
+    # Test Accuracy
+    pred = final_svd.predict(test_svd)
+    test_acc = evaluate(test_data, pred)
+    print("Final Test Accuracy: ", test_acc)
 
 
 if __name__ == '__main__':
